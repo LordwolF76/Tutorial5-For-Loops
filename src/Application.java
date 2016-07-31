8017a7b8ee5496df933f18ae0f186ac286a92320
LordwolF76 <lordwolf76@gmail.com>
2016-07-31 16:22:26 -0700

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+/**
+ * Created by LordwolF on 7/31/2016.
+ */
+public class Application {
+    public static void main(String[] args) {
+
+        for(int i = 0;i < 5; i = i + 1) {
+            System.out.printf("The value of i is: %d\n", i);
+        }
+    }
+}
